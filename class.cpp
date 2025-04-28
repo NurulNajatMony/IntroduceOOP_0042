504ccca6b28c323c8a54769a55482b5702222bef
NurulNajatMony <nurulmony505@gmail.com>
2025-04-28 11:07:56 +0700

    membuat class public untuk input data

--- class.cpp
+++ class.cpp
@@ -10,5 +10,16 @@ class Mobil
 private:
     int harga_mobil;
 
+public:
+    void input_data(){
+        cout << "Masukkan nama mobil : ";
+        cin >> nama_mobil;
+        cout << "Masukkan tipe mobil : ";
+        cin >> tipe_mobil;
+        cout << "Masukkan harga mobil : ";
+        cin >> harga_mobil;
+    }
+
+
 };
 
